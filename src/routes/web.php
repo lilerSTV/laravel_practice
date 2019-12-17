393c1d480b2b1e2c45e07e1a71e0a1321973e8cb
itce01 <itce01@PC0070.local>
2019-12-17 16:38:02 +0900

    フォルダを作成した

--- src/routes/web.php
+++ src/routes/web.php
@@ -11,6 +11,17 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+
+    if($validator->fails()){
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+
+    //TODO: Add New Task
+
 });
